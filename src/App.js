cebcc7f5084c649cf3f9f1ade08e47d35453787a
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2022-07-29 20:54:01 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Button from './components/Button/Button'
+import { ButtonAnother } from './components/Button/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>test</div>
+      <Button />
+      <ButtonAnother />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const Button = () => {
+    const btnName = "hey"
+    return <button>{ btnName }</button>
+};
+
+export const ButtonAnother = () => {
+    const btnName = "hey"
+    return <button>{ btnName } offer</button>
+};
+
+export default Button;
